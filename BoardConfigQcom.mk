LineageOS/android_vendor_lineage

Move msm8998 to 4.14
Change-Id: I7b8cd615737d41b597d234852c4124dab14087f1

config/BoardConfigQcom.mk
@@ -11,7 +11,6 @@ HOLI := holi #SM4350
 TARO := taro #SM8450
 
 UM_3_18_FAMILY := msm8996
-UM_4_4_FAMILY := msm8998
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_FAMILY := $(KONA) $(LITO) $(BENGAL)
@@ -20,9 +19,10 @@ UM_5_10_FAMILY := $(TARO)
 
 ifeq (,$(TARGET_ENFORCES_QSSI))
 UM_3_18_FAMILY += msm8937 msm8953
-UM_4_4_FAMILY += sdm660
+UM_4_4_FAMILY := sdm660 msm8998
 else
 UM_4_9_LEGACY_FAMILY := msm8937 msm8953
+UM_4_14_LEGACY_FAMILY := msm8998
 UM_4_19_LEGACY_FAMILY := sdm660
 endif
 
@@ -30,6 +30,7 @@ UM_PLATFORMS := \
     $(UM_3_18_FAMILY) \
     $(UM_4_9_LEGACY_FAMILY) \
     $(UM_4_4_FAMILY) \
+    $(UM_4_14_LEGACY_FAMILY) \
     $(UM_4_19_LEGACY_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
@@ -47,6 +48,7 @@ LEGACY_UM_PLATFORMS := \
 
 QSSI_SUPPORTED_PLATFORMS := \
     $(UM_4_9_LEGACY_FAMILY) \
+    $(UM_4_14_LEGACY_FAMILY) \
     $(UM_4_19_LEGACY_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
@@ -140,6 +142,9 @@ else ifneq ($(filter $(UM_4_9_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := msm8953
 else ifneq ($(filter $(UM_4_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_4_FAMILY)
+    QCOM_HARDWARE_VARIANT := msm8998
+else ifneq ($(filter $(UM_4_14_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    MSM_VIDC_TARGET_LIST := $(UM_4_14_LEGACY_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8998
 else ifneq ($(filter $(UM_4_19_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_19_LEGACY_FAMILY)

LeafOS-experimental/android_hardware_qcom-caf_common

BoardConfigQcom.mk
@@ -6,9 +6,10 @@ UM_4_4_HAL_FAMILY := msm8998
 
 ifeq (,$(TARGET_ENFORCES_QSSI))
 UM_3_18_HAL_FAMILY += msm8937 msm8953
-UM_4_4_HAL_FAMILY += sdm660
+UM_4_4_HAL_FAMILY := sdm660 msm8998
 else
 UM_4_9_LEGACY_FAMILY := msm8937 msm8953
+UM_4_14_LEGACY_FAMILY := msm8998
 UM_4_19_LEGACY_FAMILY := sdm660
 endif
 
@@ -32,6 +33,7 @@ LEGACY_UM_PLATFORMS := \
 
 QSSI_SUPPORTED_PLATFORMS := \
     $(UM_4_9_LEGACY_FAMILY) \
+    $(UM_4_14_LEGACY_FAMILY) \
     $(UM_4_19_LEGACY_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
@@ -151,6 +153,9 @@ else ifneq ($(filter $(UM_4_9_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := msm8953
 else ifneq ($(filter $(UM_4_4_HAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_4_HAL_FAMILY)
+    QCOM_HARDWARE_VARIANT := msm8998
+else ifneq ($(filter $(UM_4_14_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    MSM_VIDC_TARGET_LIST := $(UM_4_14_LEGACY_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8998
 else ifneq ($(filter $(UM_4_19_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_19_LEGACY_FAMILY)